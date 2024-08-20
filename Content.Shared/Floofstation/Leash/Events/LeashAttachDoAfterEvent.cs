564b7febd6dd1460782321d98ec0a5e701e9595f
fox <daytimer253@gmail.com>
2024-08-20 11:39:25 +0300

    Shuffle files around

--- Content.Shared/Floofstation/Leash/Events/LeashAttachDoAfterEvent.cs
+++
@@ -1,9 +0,0 @@
-using Content.Shared.DoAfter;
-using Robust.Shared.Serialization;
-
-namespace Content.Shared.Floofstation.Leash.Events;
-
-[Serializable, NetSerializable]
-public sealed partial class LeashAttachDoAfterEvent : SimpleDoAfterEvent
-{
-}
--- Content.Shared/Floofstation/Leash/LeashDoAfterEvents.cs
+++ Content.Shared/Floofstation/Leash/LeashDoAfterEvents.cs
@@ -1,7 +1,12 @@
 using Content.Shared.DoAfter;
 using Robust.Shared.Serialization;
 
-namespace Content.Shared.Floofstation.Leash.Events;
+namespace Content.Shared.Floofstation.Leash;
+
+[Serializable, NetSerializable]
+public sealed partial class LeashAttachDoAfterEvent : SimpleDoAfterEvent
+{
+}
 
 [Serializable, NetSerializable]
 public sealed partial class LeashDetachDoAfterEvent : SimpleDoAfterEvent
