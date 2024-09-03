a06dbc7077e8553b162ec0e4a2fcffb45e2a19f6
Pierson Arnold <greyalphawolf7@gmail.com>
2024-09-03 16:11:07 -0500

    Test Satisfy

--- Content.IntegrationTests/Tests/Preferences/ServerDbSqliteTests.cs
+++ Content.IntegrationTests/Tests/Preferences/ServerDbSqliteTests.cs
@@ -41,6 +41,7 @@ namespace Content.IntegrationTests.Tests.Preferences
                 "Charlie Charlieson",
                 "The biggest boy around.",
                 "Human",
+                "Everything",
                 1,
                 1,
                 21,
