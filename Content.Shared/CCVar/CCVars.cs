eb800c6a12bcec07e56cb32da0059dd9f456a279
sleepyyapril <flyingkarii@gmail.com>
2024-11-12 03:15:47 +0100

    CVars

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -2005,7 +2005,7 @@ namespace Content.Shared.CCVar
         /// Allow Ethereal Ent to PassThrough Walls/Objects while in Ethereal.
         /// </summary>
         public static readonly CVarDef<bool> EtherealPassThrough =
-            CVarDef.Create("ic.EtherealPassThrough", true, CVar.SERVER);
+            CVarDef.Create("ic.EtherealPassThrough", false, CVar.SERVER);
 
         /*
          * Salvage
@@ -2062,13 +2062,13 @@ namespace Content.Shared.CCVar
          */
 
         /// <summary>
-        /// Time that players have to wait before rules can be accepted.
+        ///     Time that players have to wait before rules can be accepted.
         /// </summary>
         public static readonly CVarDef<float> RulesWaitTime =
-            CVarDef.Create("rules.time", 60f, CVar.SERVER | CVar.REPLICATED);
+            CVarDef.Create("rules.time", 10f, CVar.SERVER | CVar.REPLICATED);
 
         /// <summary>
-        /// Don't show rules to localhost/loopback interface.
+        ///     Don't show rules to localhost/loopback interface.
         /// </summary>
         public static readonly CVarDef<bool> RulesExemptLocal =
             CVarDef.Create("rules.exempt_local", true, CVar.SERVERONLY);
@@ -2640,5 +2640,33 @@ namespace Content.Shared.CCVar
             CVarDef.Create("ghost.allow_same_character", false, CVar.SERVERONLY);
 
         #endregion
+
+        /// <summary>
+        /// Set to true to disable parallel processing in the pow3r solver.
+        /// </summary>
+        public static readonly CVarDef<bool> DebugPow3rDisableParallel =
+            CVarDef.Create("debug.pow3r_disable_parallel", true, CVar.SERVERONLY);
+
+        /*
+        * AUTOVOTE SYSTEM
+        */
+
+        /// <summary>
+        ///     Enables the automatic voting system.
+        /// </summary>
+        public static readonly CVarDef<bool> AutoVoteEnabled =
+            CVarDef.Create("vote.autovote_enabled", false, CVar.SERVERONLY);
+
+        /// <summary>
+        ///     Automatically make map votes on return to lobby? Requires auto voting to be enabled.
+        /// </summary>
+        public static readonly CVarDef<bool> MapAutoVoteEnabled =
+            CVarDef.Create("vote.map_autovote_enabled", true, CVar.SERVERONLY);
+
+        /// <summary>
+        ///     Automatically make preset votes on return to lobby? Requires auto voting to be enabled.
+        /// </summary>
+        public static readonly CVarDef<bool> PresetAutoVoteEnabled =
+            CVarDef.Create("vote.preset_autovote_enabled", true, CVar.SERVERONLY);
     }
 }
