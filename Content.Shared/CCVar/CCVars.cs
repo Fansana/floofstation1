dfbbd99ed7a95197eecdd3b14c6fae64c5fba3e4
Fansana <fansana95@googlemail.com>
2024-12-14 17:03:52 +0100

    update summary

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -2645,17 +2645,21 @@ namespace Content.Shared.CCVar
         * AUTOVOTE SYSTEM
         */
 
-        /// Enables the automatic voting system.
+        /// <summary>
+        ///     Enables the automatic voting system.
+        /// </summary>
         public static readonly CVarDef<bool> AutoVoteEnabled =
-            CVarDef.Create("vote.autovote_enabled", true, CVar.SERVERONLY); // Floof 
+            CVarDef.Create("vote.autovote_enabled", true, CVar.SERVERONLY); // Floof
 
-        /// Automatically starts a map vote when returning to the lobby.
-        /// Requires auto voting to be enabled.
+        /// <summary>
+        ///     Automatically make map votes on return to lobby? Requires auto voting to be enabled.
+        /// </summary>
         public static readonly CVarDef<bool> MapAutoVoteEnabled =
             CVarDef.Create("vote.map_autovote_enabled", true, CVar.SERVERONLY);
 
-        /// Automatically starts a gamemode vote when returning to the lobby.
-        /// Requires auto voting to be enabled.
+        /// <summary>
+        ///     Automatically make preset votes on return to lobby? Requires auto voting to be enabled.
+        /// </summary>
         public static readonly CVarDef<bool> PresetAutoVoteEnabled =
             CVarDef.Create("vote.preset_autovote_enabled", true, CVar.SERVERONLY);
     }
