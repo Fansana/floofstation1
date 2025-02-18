58945d70eeedacf4ec61c3e131d957188b21a288
LucasTheDrgn <kirbyfan.95@gmail.com>
2025-02-18 00:46:32 -0500

    consent menu fixes

--- Content.Client/Consent/UI/Windows/ConsentWindow.xaml.cs
+++ Content.Client/Consent/UI/Windows/ConsentWindow.xaml.cs
@@ -157,7 +157,10 @@ public sealed partial class ConsentWindow : FancyWindow
 
         _entries.Clear();
 
-        var consentprototypelist = _protoManager.EnumeratePrototypes<ConsentTogglePrototype>();
+        var consentprototypelist = new List<ConsentTogglePrototype>(_protoManager.EnumeratePrototypes<ConsentTogglePrototype>());
+
+        consentprototypelist.Sort();
+
         foreach (var prototype in consentprototypelist)
             AddConsentEntry(prototype);
 
