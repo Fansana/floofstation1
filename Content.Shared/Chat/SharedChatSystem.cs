55b7c91069129a194a309341047258269d769331
Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>
2025-02-23 21:58:02 +0300

    Change SLOOC prefix

--- Content.Shared/Chat/SharedChatSystem.cs
+++ Content.Shared/Chat/SharedChatSystem.cs
@@ -21,7 +21,7 @@ public abstract class SharedChatSystem : EntitySystem
     public const char EmotesPrefix = '@';
     public const char EmotesAltPrefix = '*';
     public const char SubtlePrefix = '-';
-    public const char SubtleOOCPrefix = '.';
+    public const char SubtleOOCPrefix = '{'; // Floof - changed to "{"
     public const char AdminPrefix = ']';
     public const char WhisperPrefix = ',';
     public const char TelepathicPrefix = '='; //Nyano - Summary: Adds the telepathic channel's prefix.
