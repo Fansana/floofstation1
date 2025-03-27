7e5a5631e3fde5e38c176e9a2689a7f1331cf205
fenndragon <fenndragon@gmail.com>
2025-03-27 00:12:56 -0600

    revert 2

--- Content.Server/FloofStation/Traits/TraitSystem.Functions.cs
+++ Content.Server/FloofStation/Traits/TraitSystem.Functions.cs
@@ -1,20 +1,25 @@
 using Content.Shared.Traits;
 using Robust.Shared.Serialization.Manager;
-using Content.Shared.HeightAdjust;
+using Robust.Server.GameObjects;
+using System.Numerics;
 
 namespace Content.Server.FloofStation.Traits;
 
-// Scales/modifies the size of the character using the Floofstation modified heightAdjustSystem function SetScale
-public sealed partial class TraitSetScale : TraitFunction 
+// Scales/modifies the visual size of the character. DOES NOT MODIFY DENSITY/WEIGHT. Use in conjunction with FixtureDensityModifier component.
+public sealed partial class TraitScaleVisualSize : TraitFunction 
 {
     [DataField]
-    public float scale;
+    public Vector2 scale;
 
     public override void OnPlayerSpawn(EntityUid uid,
         IComponentFactory factory,
         IEntityManager entityManager,
         ISerializationManager serializationManager)
-    { 
-        entityManager.System<HeightAdjustSystem>().SetScale(uid, scale, restricted: false);
+    {
+       // Make sure the player has the ScaleVisuals Component, it's required to activate the ScaleVisualsSystem
+       entityManager.EnsureComponent<ScaleVisualsComponent>(uid);
+       
+       // Apply the new scale, generate the AppearanceComponent if required
+       entityManager.System<AppearanceSystem>().SetData(uid, ScaleVisuals.Scale, scale, entityManager.EnsureComponent<AppearanceComponent>(uid));
     }
 }
