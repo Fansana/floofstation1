185cbca4565116e020e8c6140aa9daa4cc0e25ce
Mnemotechnician <69920617+mnemotechnician@users.noreply.github.com>
2025-03-30 22:59:00 +0300

    Move unrevivable to shared

---
+++ Content.Shared/Traits/Assorted/Components/UnrevivableComponent.cs
@@ -0,0 +1,8 @@
+namespace Content.Shared.Traits.Assorted.Components;
+
+// Floofstation - moved to shared namespace
+/// <summary>
+/// This is used for the urevivable trait.
+/// </summary>
+[RegisterComponent]
+public sealed partial class UnrevivableComponent : Component;
