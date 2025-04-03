2c33317f83e844c2759ac6a1ff7496934fb9b4ad
FoxxoTrystan <45297731+FoxxoTrystan@users.noreply.github.com>
2025-04-03 18:06:58 +0200

    Update CCVars.cs

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -1581,6 +1581,12 @@ namespace Content.Shared.CCVar
         public static readonly CVarDef<string> WhitelistPrototypeList =
             CVarDef.Create("whitelist.prototype_list", "basicWhitelist", CVar.SERVERONLY);
 
+        /// <summary>
+        ///     The loc string to display as a disconnect reason when someone is not whitelisted.
+        /// </summary>
+        public static readonly CVarDef<string> WhitelistReason =
+            CVarDef.Create("whitelist.reason", "whitelist-not-whitelisted", CVar.SERVERONLY);
+
         /*
          * VOTE
          */
