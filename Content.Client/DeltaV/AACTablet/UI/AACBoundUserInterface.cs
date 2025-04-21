4b3f388b916ffc3561659d40a49fb9661ce8c268
M3739 <47579354+M3739@users.noreply.github.com>
2025-04-21 12:54:22 -0400

    Comment updates for #701

--- Content.Client/DeltaV/AACTablet/UI/AACBoundUserInterface.cs
+++ Content.Client/DeltaV/AACTablet/UI/AACBoundUserInterface.cs
@@ -1,6 +1,6 @@
-using Content.Shared.DeltaV.AACTablet; // Floof - M3739 - DeltaV namespace
-using Content.Shared.DeltaV.QuickPhrase; // Floof - M3739 - DeltaV namespace
-// using Robust.Client.UserInterface; | Floof - M3739 - This relies on a version of Robust Toolbox we don't have. 3/31/2025
+using Content.Shared.DeltaV.AACTablet; // Floof - M3739 - #701 - DeltaV namespace
+using Content.Shared.DeltaV.QuickPhrase; // Floof - M3739 - #701 - DeltaV namespace
+// using Robust.Client.UserInterface; | Floof - M3739 - #701 - This relies on a version of Robust Toolbox we don't have. 3/31/2025
 using Robust.Shared.Prototypes;
 
 namespace Content.Client.DeltaV.AACTablet.UI;
@@ -18,10 +18,10 @@ public sealed class AACBoundUserInterface : BoundUserInterface
     {
         base.Open();
         _window?.Close();
-        _window = new AACWindow(); // Floof - M3739 - CreateWindow relies on a version of Robust toolbox we don't have. 3/31/2025
-        _window.OpenCentered(); // Floof - M3739 - That being said, we have to rely on the way we do this.
+        _window = new AACWindow(); // Floof - M3739 - #701 - CreateWindow relies on a version of Robust toolbox we don't have. 3/31/2025
+        _window.OpenCentered(); // Floof - M3739 - #701 - That being said, we have to rely on the way we do this.
         _window.PhraseButtonPressed += OnPhraseButtonPressed;
-        _window.OnClose += Close; // Floof
+        _window.OnClose += Close; // Floof - M3739 - #701
     }
 
     private void OnPhraseButtonPressed(List<ProtoId<QuickPhrasePrototype>> phraseId)
@@ -29,7 +29,7 @@ public sealed class AACBoundUserInterface : BoundUserInterface
         SendMessage(new AACTabletSendPhraseMessage(phraseId));
     }
 
-    protected override void Dispose(bool disposing) // Floof
+    protected override void Dispose(bool disposing) // Floof - M3739 - #701
     {
         base.Dispose(disposing);
         _window?.Orphan();
