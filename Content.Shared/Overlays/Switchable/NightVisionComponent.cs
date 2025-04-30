6e58f7115343d7bb806d95de9d10846676383d8e
M3739 <47579354+M3739@users.noreply.github.com>
2025-04-29 22:51:42 -0400

    Component variables, name corrections
    Special thanks to Floofmin!

--- Content.Shared/Overlays/Switchable/NightVisionComponent.cs
+++ Content.Shared/Overlays/Switchable/NightVisionComponent.cs
@@ -9,6 +9,20 @@ public sealed partial class NightVisionComponent : SwitchableOverlayComponent
     public override string? ToggleAction { get; set; } = "ToggleNightVision";
 
     public override Color Color { get; set; } = Color.FromHex("#98FB98");
+    
+    /// <summary>
+    ///     Determines how intense the visual artifacting will be.
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadOnly)]
+    public float NvdSnow { get; set; } = 1f;
+
+    /// <summary>
+    ///     Determines whether or not the night vision will only see in greyscale.
+    ///     This means it will only see in shades of it's color tint.
+    ///     1 for on, 0 for off.
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadOnly)]
+    public float Darkvision { get; set; } = 1f;
 }
 
 public sealed partial class ToggleNightVisionEvent : InstantActionEvent;
