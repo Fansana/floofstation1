366a9e485b6be54135a8f69a771d564511e4ec7e
Raikyr0 <Kurohana@hotmail.com.au>
2025-05-04 04:39:33 +1000

    Update NPCBlackboard.cs

--- Content.Server/NPC/NPCBlackboard.cs
+++ Content.Server/NPC/NPCBlackboard.cs
@@ -27,6 +27,8 @@ public sealed partial class NPCBlackboard : IEnumerable<KeyValuePair<string, obj
         {"MaximumIdleTime", 7f},
         {MedibotInjectRange, 4f},
         {MeleeMissChance, 0.3f},
+
+        {PlantbotServiceRange, 4f},
         {"MeleeRange", 1f},
         {"MinimumIdleTime", 2f},
         {"MovementRangeClose", 0.2f},
