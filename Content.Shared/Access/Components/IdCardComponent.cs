7fc6990469b9a9cd72a5bad06c1a97d23d00fc8f
Mnemotechnician <69920617+mnemotechnician@users.noreply.github.com>
2025-05-09 13:48:41 +0300

    Add VV to IdCardComponent.LocalizedJobTitle

--- Content.Shared/Access/Components/IdCardComponent.cs
+++ Content.Shared/Access/Components/IdCardComponent.cs
@@ -23,6 +23,7 @@ public sealed partial class IdCardComponent : Component
 
     private string? _jobTitle;
 
+    [ViewVariables] // Floof
     [Access(typeof(SharedIdCardSystem), typeof(SharedPdaSystem), typeof(SharedAgentIdCardSystem), Other = AccessPermissions.ReadWriteExecute)]
     public string? LocalizedJobTitle { set => _jobTitle = value; get => _jobTitle ?? Loc.GetString(JobTitle ?? string.Empty); }
 
