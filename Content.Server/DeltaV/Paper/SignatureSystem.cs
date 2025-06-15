LaCumbiaDelCoronavirus/KS16

Devil Antagonist  (#2315)
* Baseline

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* revival contract & other stuff

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* handshake & stuff

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* bweh

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* posession shitcode
deepseek, save me deepseek :pray:

goob is kil

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* other improvements

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* devil weakness & stuff

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* i forgot to commit the textures...

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* power level stuff

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* committing so i can push to origin

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* bweh

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* coderabbit suggestions

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* do NOT pass go.
do NOT collect 200.
go STRAIGHT to hell.

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* possession actually works, wazow

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* genericized le systema

she five on my pebbles until i rainworld

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* removed testing ability

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* Genericized possession, now you can reuse this for revenants or whatever!

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* fixed thingy

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

* crematorium immune + banishment
remind me to update the guidebook later

Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>

Content.Server/_DV/Paper/SignatureSystem.cs
@@ -3,6 +3,8 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+using Content.Goobstation.Common.Paper;
+using Content.Goobstation.Shared.Devil;
 using Content.Server.Access.Systems;
 using Content.Server.Popups;
 using Content.Shared.Paper;
@@ -54,7 +56,7 @@ public sealed class SignatureSystem : EntitySystem
     }
 
     /// <summary>
-    ///     Tries add add a signature to the paper with signer's name.
+    ///     Tries to add a signature to the paper with signer's name.
     /// </summary>
     public bool TrySignPaper(Entity<PaperComponent> paper, EntityUid signer, EntityUid pen)
     {
@@ -63,6 +65,11 @@ public sealed class SignatureSystem : EntitySystem
         var ev = new SignAttemptEvent(paper, signer);
         RaiseLocalEvent(pen, ref ev);
         if (ev.Cancelled)
+            return false;
+
+        var paperEvent = new BeingSignedAttemptEvent(paper, signer); // Goobstation
+        RaiseLocalEvent(paper.Owner, ref paperEvent);
+        if (paperEvent.Cancelled)
             return false;
 
         var signatureName = DetermineEntitySignature(signer);
@@ -76,15 +83,21 @@ public sealed class SignatureSystem : EntitySystem
         if (!comp.StampedBy.Contains(stampInfo) && _paper.TryStamp(paper, stampInfo, SignatureStampState))
         {
             // Show popups and play a paper writing sound
-            var signedOtherMessage = Loc.GetString("paper-signed-other", ("user", signer), ("target", paper.Owner));
-            _popup.PopupEntity(signedOtherMessage, signer, Filter.PvsExcept(signer, entityManager: EntityManager), true);
+            if (!HasComp<DevilComponent>(signer)) // Goobstation - Don't display popups for devils, it covers the others.
+            {
+                var signedOtherMessage = Loc.GetString("paper-signed-other", ("user", signer), ("target", paper.Owner));
+                _popup.PopupEntity(signedOtherMessage, signer, Filter.PvsExcept(signer, entityManager: EntityManager), true);
 
-            var signedSelfMessage = Loc.GetString("paper-signed-self", ("target", paper.Owner));
-            _popup.PopupEntity(signedSelfMessage, signer, signer);
+                var signedSelfMessage = Loc.GetString("paper-signed-self", ("target", paper.Owner));
+                _popup.PopupEntity(signedSelfMessage, signer, signer);
+            }
 
             _audio.PlayPvs(comp.Sound, signer);
 
             _paper.UpdateUserInterface(paper);
+
+            var evSignSucessfulEvent = new SignSuccessfulEvent(paper, signer); // Goobstation - Devil Antagonist
+            RaiseLocalEvent(paper, ref evSignSucessfulEvent); // Goobstation - Devil Antagonist
 
             return true;
         }
@@ -99,6 +112,10 @@ public sealed class SignatureSystem : EntitySystem
 
     private string DetermineEntitySignature(EntityUid uid)
     {
+        // Goobstation - Allow devils to sign their true name.
+        if (TryComp<DevilComponent>(uid, out var devilComp) && !string.IsNullOrWhiteSpace(devilComp.TrueName))
+            return devilComp.TrueName;
+
         // If the entity has an ID, use the name on it.
         if (_idCard.TryFindIdCard(uid, out var id) && !string.IsNullOrWhiteSpace(id.Comp.FullName))
             return id.Comp.FullName;

Content.Goobstation.Server/Devil/Condemned/CondemnedComponent.cs
@@ -1,0 +1,74 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Goobstation.Server.Devil.Condemned;
+
+/// <summary>
+/// Marks an entity as having sold their soul.
+/// When you die, do NOT collect 200, do NOT pass go. Go directly to hell
+/// </summary>
+[RegisterComponent]
+public sealed partial class CondemnedComponent : Component
+{
+    /// <summary>
+    /// The current phase of the condemnation animation.
+    /// </summary>
+    [DataField]
+    public CondemnedSystem.CondemnedPhase CurrentPhase = CondemnedSystem.CondemnedPhase.Waiting;
+
+    /// <summary>
+    /// Who owns this entities soul
+    /// </summary>
+    [DataField]
+    public EntityUid SoulOwner;
+
+    /// <summary>
+    /// The elapsed time of the phase.
+    /// </summary>
+    [DataField]
+    public float PhaseTimer;
+
+    /// <summary>
+    /// How long the hand effect will last
+    /// </summary>
+    [DataField]
+    public float HandDuration;
+
+    /// <summary>
+    /// Should the examine message show when examining someone with this component?
+    /// </summary>
+    [DataField("showExamine")]
+    public bool ShowExamineMessage = true;
+
+    /// <summary>
+    /// Is this entities soul owned, but not by a devil?
+    /// </summary>
+    [DataField]
+    public bool SoulOwnedNotDevil = false;
+
+    /// <summary>
+    /// Should this entity be sent to hell on death?
+    /// </summary>
+    [DataField]
+    public bool CondemnOnDeath = true;
+
+    /// <summary>
+    /// Was this target already weak to holy before becoming condemned?
+    /// </summary>
+    [DataField]
+    public bool WasWeakToHoly;
+
+    /// <summary>
+    /// Should movement be locked during the animation?
+    /// </summary>
+    [DataField]
+    public bool FreezeDuringCondemnation;
+
+    /// <summary>
+    /// Should this entity be banished (sent to limbo for several minutes) or should they just be deleted?
+    /// </summary>
+    [DataField]
+    public CondemnedSystem.CondemnedBehavior CondemnedBehavior = CondemnedSystem.CondemnedBehavior.Delete;
+}

Content.Goobstation.Server/Devil/Contract/Revival/RevivalContractComponent.cs
@@ -1,0 +1,23 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Goobstation.Server.Devil.Contract.Revival;
+
+[RegisterComponent]
+public sealed partial class RevivalContractComponent : Component
+{
+    /// <summary>
+    /// The entity who signed the paper, AKA, the entity who has the effects applied.
+    /// </summary>
+    [DataField]
+    public EntityUid? Signer;
+
+    /// <summary>
+    /// The entity who created the contract, AKA, the entity who gains the soul.
+    /// </summary>
+    [DataField]
+    public EntityUid? ContractOwner;
+
+}

Content.Goobstation.Server/GameTicking/Rules/DevilRuleSystem.cs
@@ -1,0 +1,106 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using System.Text;
+using Content.Goobstation.Server.Devil;
+using Content.Goobstation.Server.Devil.Roles;
+using Content.Goobstation.Shared;
+using Content.Goobstation.Shared.Devil;
+using Content.Server.Antag;
+using Content.Server.Bible.Components;
+using Content.Server.GameTicking.Rules;
+using Content.Server.Mind;
+using Content.Server.Objectives;
+using Content.Server.Roles;
+using Content.Shared.NPC.Prototypes;
+using Content.Shared.NPC.Systems;
+using Content.Shared.Roles;
+using Content.Shared.Store;
+using Content.Shared.Store.Components;
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+
+namespace Content.Goobstation.Server.GameTicking.Rules;
+
+public sealed class DevilRuleSystem : GameRuleSystem<DevilRuleComponent>
+{
+    [Dependency] private readonly MindSystem _mind = default!;
+    [Dependency] private readonly AntagSelectionSystem _antag = default!;
+    [Dependency] private readonly SharedRoleSystem _role = default!;
+    [Dependency] private readonly NpcFactionSystem _npcFaction = default!;
+    [Dependency] private readonly ObjectivesSystem _objective = default!;
+
+    private readonly SoundSpecifier _briefingSound = new SoundPathSpecifier("/Audio/_Goobstation/Ambience/Antag/devil_start.ogg");
+
+    [ValidatePrototypeId<NpcFactionPrototype>]
+    private const string DevilFaction = "DevilFaction";
+
+    private readonly EntProtoId _devilMindRole = "DevilMindRole";
+
+    [ValidatePrototypeId<NpcFactionPrototype>]
+    private const string NanotrasenFaction = "NanoTrasen";
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<DevilRuleComponent, AfterAntagEntitySelectedEvent>(OnSelectAntag);
+        SubscribeLocalEvent<DevilRuleComponent, ObjectivesTextPrependEvent>(OnTextPrepend);
+    }
+
+    private void OnSelectAntag(EntityUid uid, DevilRuleComponent comp, ref AfterAntagEntitySelectedEvent args)
+    {
+        MakeDevil(args.EntityUid, comp);
+    }
+
+    private bool MakeDevil(EntityUid target, DevilRuleComponent rule)
+    {
+        if (!_mind.TryGetMind(target, out var mindId, out var mind))
+            return false;
+
+        _role.MindAddRole(mindId, _devilMindRole.Id, mind, true);
+
+        var devilComp = EnsureComp<DevilComponent>(target);
+
+        var meta = MetaData(target);
+
+        var briefing = Loc.GetString("devil-role-greeting", ("trueName", devilComp.TrueName), ("playerName", meta.EntityName));
+
+        _antag.SendBriefing(target, briefing, Color.DarkRed, _briefingSound);
+
+        if (_role.MindHasRole<DevilRoleComponent>(mindId, out var mr))
+            AddComp(mr.Value, new RoleBriefingComponent { Briefing = briefing }, overwrite: true);
+
+        _npcFaction.RemoveFaction(target, NanotrasenFaction);
+        _npcFaction.AddFaction(target, DevilFaction);
+
+        return true;
+    }
+
+    private void OnTextPrepend(EntityUid uid, DevilRuleComponent comp, ref ObjectivesTextPrependEvent args)
+    {
+        var mostContractsName = string.Empty;
+        var mostContracts = 0f;
+
+        foreach (var devil in EntityQuery<DevilComponent>())
+        {
+            if (!_mind.TryGetMind(devil.Owner, out var mindId, out var mind))
+                continue;
+
+            var metaData = MetaData(devil.Owner);
+
+            if (devil.Souls > mostContracts)
+            {
+                mostContracts = devil.Souls;
+                mostContractsName = _objective.GetTitle((mindId, mind), metaData.EntityName);
+            }
+        }
+
+        var sb = new StringBuilder();
+        sb.AppendLine(Loc.GetString($"roundend-prepend-devil-contracts{(!string.IsNullOrWhiteSpace(mostContractsName) ? "-named" : "")}", ("name", mostContractsName), ("number", mostContracts)));
+
+        args.Text = sb.ToString();
+    }
+}

Content.Goobstation.Server/Possession/PossessedComponent.cs
@@ -1,0 +1,47 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Shared.Mind;
+using Robust.Shared.Audio;
+
+namespace Content.Goobstation.Server.Possession;
+
+
+[RegisterComponent]
+public sealed partial class PossessedComponent : Component
+{
+    [DataField]
+    public EntityUid OriginalMindId;
+
+    [DataField]
+    public EntityUid OriginalEntity;
+
+    [DataField]
+    public MindComponent OriginalMindComponent;
+
+    [DataField]
+    public EntityUid PossessorMindId;
+
+    [DataField]
+    public EntityUid PossessorOriginalEntity;
+
+    [DataField]
+    public MindComponent PossessorMindComponent;
+
+    [DataField]
+    public TimeSpan PossessionEndTime;
+
+    [DataField]
+    public TimeSpan PossessionTimeRemaining;
+
+    [DataField]
+    public bool WasPacified;
+
+    [DataField]
+    public bool DoPacify;
+
+    [DataField]
+    public SoundPathSpecifier PossessionSoundPath = new ("/Audio/_Goobstation/Effects/bone_crack.ogg");
+}

Content.Goobstation.Common/Paper/BeingSignedAttemptEvent.cs
@@ -1,0 +1,12 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Goobstation.Common.Paper;
+
+/// <summary>
+/// 	Raised on the paper when a sign is attempted
+/// </summary>
+[ByRefEvent]
+public record struct BeingSignedAttemptEvent(EntityUid Paper, EntityUid Signer, bool Cancelled = false);

Content.Goobstation.Common/Paper/SignSucessfulEvent.cs
@@ -1,0 +1,12 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Goobstation.Common.Paper;
+
+/// <summary>
+/// 	Raised on the paper when a sign is successful.
+/// </summary>
+[ByRefEvent]
+public record struct SignSuccessfulEvent(EntityUid Paper, EntityUid User);

Fansana/floofstation1

Content.Server/DeltaV/Paper/SignatureSystem.cs
@@ -1,3 +1,5 @@
+using Content.Shared._Goobstation.Paper;
+using Content.Shared._Goobstation.Devil;
 using Content.Server.Access.Systems;
 using Content.Server.Paper;
 using Content.Server.Popups;
@@ -49,7 +51,7 @@ public sealed class SignatureSystem : EntitySystem
     }
 
     /// <summary>
-    ///     Tries add add a signature to the paper with signer's name.
+    ///     Tries to add a signature to the paper with signer's name.
     /// </summary>
     public bool TrySignPaper(Entity<PaperComponent> paper, EntityUid signer, EntityUid pen)
     {
@@ -58,6 +60,11 @@ public sealed class SignatureSystem : EntitySystem
         var ev = new SignAttemptEvent(paper, signer);
         RaiseLocalEvent(pen, ref ev);
         if (ev.Cancelled)
+            return false;
+
+        var paperEvent = new BeingSignedAttemptEvent(paper, signer); // Goobstation
+        RaiseLocalEvent(paper.Owner, ref paperEvent);
+        if (paperEvent.Cancelled)
             return false;
 
         var signatureName = DetermineEntitySignature(signer);
@@ -71,15 +78,20 @@ public sealed class SignatureSystem : EntitySystem
         if (!comp.StampedBy.Contains(stampInfo) && _paper.TryStamp(paper, stampInfo, SignatureStampState, comp))
         {
             // Show popups and play a paper writing sound
-            var signedOtherMessage = Loc.GetString("paper-signed-other", ("user", signer), ("target", paper.Owner));
-            _popup.PopupEntity(signedOtherMessage, signer, Filter.PvsExcept(signer, entityManager: EntityManager), true);
+            if (!HasComp<DevilComponent>(signer)) // Goobstation - Don't display popups for devils, it covers the others.
+            {
+                var signedOtherMessage = Loc.GetString("paper-signed-other", ("user", signer), ("target", paper.Owner));
+                _popup.PopupEntity(signedOtherMessage, signer, Filter.PvsExcept(signer, entityManager: EntityManager), true);
 
-            var signedSelfMessage = Loc.GetString("paper-signed-self", ("target", paper.Owner));
-            _popup.PopupEntity(signedSelfMessage, signer, signer);
-
+                var signedSelfMessage = Loc.GetString("paper-signed-self", ("target", paper.Owner));
+                _popup.PopupEntity(signedSelfMessage, signer, signer);
+            }
             _audio.PlayPvs(comp.Sound, signer);
 
             _paper.UpdateUserInterface(paper, comp);
+
+            var evSignSucessfulEvent = new SignSuccessfulEvent(paper, signer); // Goobstation - Devil Antagonist
+            RaiseLocalEvent(paper, ref evSignSucessfulEvent); // Goobstation - Devil Antagonist
 
             return true;
         }
@@ -94,6 +106,10 @@ public sealed class SignatureSystem : EntitySystem
 
     private string DetermineEntitySignature(EntityUid uid)
     {
+        // Goobstation - Allow devils to sign their true name.
+        if (TryComp<DevilComponent>(uid, out var devilComp) && !string.IsNullOrWhiteSpace(devilComp.TrueName))
+            return devilComp.TrueName;
+
         // If the entity has an ID, use the name on it.
         if (_idCard.TryFindIdCard(uid, out var id) && !string.IsNullOrWhiteSpace(id.Comp.FullName))
             return id.Comp.FullName;

Content.Server/_Goobstation/Devil/Condemned/CondemnedComponent.cs
@@ -1,0 +1,74 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Server._Goobstation.Devil.Condemned;
+
+/// <summary>
+/// Marks an entity as having sold their soul.
+/// When you die, do NOT collect 200, do NOT pass go. Go directly to hell
+/// </summary>
+[RegisterComponent]
+public sealed partial class CondemnedComponent : Component
+{
+    /// <summary>
+    /// The current phase of the condemnation animation.
+    /// </summary>
+    [DataField]
+    public CondemnedSystem.CondemnedPhase CurrentPhase = CondemnedSystem.CondemnedPhase.Waiting;
+
+    /// <summary>
+    /// Who owns this entities soul
+    /// </summary>
+    [DataField]
+    public EntityUid SoulOwner;
+
+    /// <summary>
+    /// The elapsed time of the phase.
+    /// </summary>
+    [DataField]
+    public float PhaseTimer;
+
+    /// <summary>
+    /// How long the hand effect will last
+    /// </summary>
+    [DataField]
+    public float HandDuration;
+
+    /// <summary>
+    /// Should the examine message show when examining someone with this component?
+    /// </summary>
+    [DataField("showExamine")]
+    public bool ShowExamineMessage = true;
+
+    /// <summary>
+    /// Is this entities soul owned, but not by a devil?
+    /// </summary>
+    [DataField]
+    public bool SoulOwnedNotDevil = false;
+
+    /// <summary>
+    /// Should this entity be sent to hell on death?
+    /// </summary>
+    [DataField]
+    public bool CondemnOnDeath = true;
+
+    /// <summary>
+    /// Was this target already weak to holy before becoming condemned?
+    /// </summary>
+    [DataField]
+    public bool WasWeakToHoly;
+
+    /// <summary>
+    /// Should movement be locked during the animation?
+    /// </summary>
+    [DataField]
+    public bool FreezeDuringCondemnation;
+
+    /// <summary>
+    /// Should this entity be banished (sent to limbo for several minutes) or should they just be deleted?
+    /// </summary>
+    [DataField]
+    public CondemnedSystem.CondemnedBehavior CondemnedBehavior = CondemnedSystem.CondemnedBehavior.Delete;
+}

Content.Server/_Goobstation/Devil/Contract/Revival/RevivalContractComponent.cs
@@ -1,0 +1,23 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Server._Goobstation.Devil.Contract.Revival;
+
+[RegisterComponent]
+public sealed partial class RevivalContractComponent : Component
+{
+    /// <summary>
+    /// The entity who signed the paper, AKA, the entity who has the effects applied.
+    /// </summary>
+    [DataField]
+    public EntityUid? Signer;
+
+    /// <summary>
+    /// The entity who created the contract, AKA, the entity who gains the soul.
+    /// </summary>
+    [DataField]
+    public EntityUid? ContractOwner;
+
+}

Content.Server/_Goobstation/GameTicking/Rules/DevilRuleSystem.cs
@@ -1,0 +1,106 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using System.Text;
+using Content.Server._Goobstation.Devil;
+using Content.Server._Goobstation.Devil.Roles;
+using Content.Shared._Goobstation;
+using Content.Shared._Goobstation.Devil;
+using Content.Server.Antag;
+using Content.Server.Bible.Components;
+using Content.Server.GameTicking.Rules;
+using Content.Server.Mind;
+using Content.Server.Objectives;
+using Content.Server.Roles;
+using Content.Shared.NPC.Prototypes;
+using Content.Shared.NPC.Systems;
+using Content.Shared.Roles;
+using Content.Shared.Store;
+using Content.Shared.Store.Components;
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._Goobstation.GameTicking.Rules;
+
+public sealed class DevilRuleSystem : GameRuleSystem<DevilRuleComponent>
+{
+    [Dependency] private readonly MindSystem _mind = default!;
+    [Dependency] private readonly AntagSelectionSystem _antag = default!;
+    [Dependency] private readonly SharedRoleSystem _role = default!;
+    [Dependency] private readonly NpcFactionSystem _npcFaction = default!;
+    [Dependency] private readonly ObjectivesSystem _objective = default!;
+
+    private readonly SoundSpecifier _briefingSound = new SoundPathSpecifier("/Audio/_Goobstation/Ambience/Antag/devil_start.ogg");
+
+    [ValidatePrototypeId<NpcFactionPrototype>]
+    private const string DevilFaction = "DevilFaction";
+
+    private readonly EntProtoId _devilMindRole = "DevilRoleComponent";
+
+    [ValidatePrototypeId<NpcFactionPrototype>]
+    private const string NanotrasenFaction = "NanoTrasen";
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<DevilRuleComponent, AfterAntagEntitySelectedEvent>(OnSelectAntag);
+        SubscribeLocalEvent<DevilRuleComponent, ObjectivesTextPrependEvent>(OnTextPrepend);
+    }
+
+    private void OnSelectAntag(EntityUid uid, DevilRuleComponent comp, ref AfterAntagEntitySelectedEvent args)
+    {
+        MakeDevil(args.EntityUid, comp);
+    }
+
+    private bool MakeDevil(EntityUid target, DevilRuleComponent rule)
+    {
+        if (!_mind.TryGetMind(target, out var mindId, out var mind))
+            return false;
+
+        _role.MindAddRole(mindId, new DevilRoleComponent(), mind, true);
+
+        var devilComp = EnsureComp<DevilComponent>(target);
+
+        var meta = MetaData(target);
+
+        var briefing = Loc.GetString("devil-role-greeting", ("trueName", devilComp.TrueName), ("playerName", meta.EntityName));
+
+        _antag.SendBriefing(target, briefing, Color.DarkRed, _briefingSound);
+
+        if (_role.MindHasRole<DevilRoleComponent>(mindId))
+            AddComp(mindId, new RoleBriefingComponent { Briefing = briefing }, overwrite: true);
+
+        _npcFaction.RemoveFaction(target, NanotrasenFaction);
+        _npcFaction.AddFaction(target, DevilFaction);
+
+        return true;
+    }
+
+    private void OnTextPrepend(EntityUid uid, DevilRuleComponent comp, ref ObjectivesTextPrependEvent args)
+    {
+        var mostContractsName = string.Empty;
+        var mostContracts = 0f;
+
+        foreach (var devil in EntityQuery<DevilComponent>())
+        {
+            if (!_mind.TryGetMind(devil.Owner, out var mindId, out var mind))
+                continue;
+
+            var metaData = MetaData(devil.Owner);
+
+            if (devil.Souls > mostContracts)
+            {
+                mostContracts = devil.Souls;
+                mostContractsName = _objective.GetTitle((mindId, mind), metaData.EntityName);
+            }
+        }
+
+        var sb = new StringBuilder();
+        sb.AppendLine(Loc.GetString($"roundend-prepend-devil-contracts{(!string.IsNullOrWhiteSpace(mostContractsName) ? "-named" : "")}", ("name", mostContractsName), ("number", mostContracts)));
+
+        args.Text = sb.ToString();
+    }
+}

Content.Server/_Goobstation/Possession/PossessedComponent.cs
@@ -1,0 +1,47 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Shared.Mind;
+using Robust.Shared.Audio;
+
+namespace Content.Server._Goobstation.Possession;
+
+
+[RegisterComponent]
+public sealed partial class PossessedComponent : Component
+{
+    [DataField]
+    public EntityUid OriginalMindId;
+
+    [DataField]
+    public EntityUid OriginalEntity;
+
+    [DataField]
+    public MindComponent OriginalMindComponent;
+
+    [DataField]
+    public EntityUid PossessorMindId;
+
+    [DataField]
+    public EntityUid PossessorOriginalEntity;
+
+    [DataField]
+    public MindComponent PossessorMindComponent;
+
+    [DataField]
+    public TimeSpan PossessionEndTime;
+
+    [DataField]
+    public TimeSpan PossessionTimeRemaining;
+
+    [DataField]
+    public bool WasPacified;
+
+    [DataField]
+    public bool DoPacify;
+
+    [DataField]
+    public SoundPathSpecifier PossessionSoundPath = new ("/Audio/_Goobstation/Effects/bone_crack.ogg");
+}

Content.Shared/_Goobstation/Paper/BeingSignedAttemptEvent.cs
@@ -1,0 +1,12 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Shared._Goobstation.Paper;
+
+/// <summary>
+/// 	Raised on the paper when a sign is attempted
+/// </summary>
+[ByRefEvent]
+public record struct BeingSignedAttemptEvent(EntityUid Paper, EntityUid Signer, bool Cancelled = false);

Content.Shared/_Goobstation/Paper/SignSucessfulEvent.cs
@@ -1,0 +1,12 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Shared._Goobstation.Paper;
+
+/// <summary>
+/// 	Raised on the paper when a sign is successful.
+/// </summary>
+[ByRefEvent]
+public record struct SignSuccessfulEvent(EntityUid Paper, EntityUid User);