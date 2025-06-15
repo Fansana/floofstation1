78d6a479ea7229ef50e2ac4197be28e6f0d1d22d
Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>
2025-06-15 18:56:55 +0300

    Fix roundstart loop caused by invalid loadouts

--- Content.Server/Clothing/Systems/LoadoutSystem.cs
+++ Content.Server/Clothing/Systems/LoadoutSystem.cs
@@ -90,7 +90,13 @@ public sealed class LoadoutSystem : EntitySystem
         }
 
         foreach (var loadout in allLoadouts)
+        #if EXCEPTION_TOLERANCE
+        try // Floofstation - try-catch in exception tolerance mode
+        #endif
         {
+            if (!Exists(loadout.Item1))
+                continue; // Floofstation - skip failed loadouts. This used to cause roundstart loop.
+            
             var loadoutProto = _protoMan.Index<LoadoutPrototype>(loadout.Item2.LoadoutName);
             if (loadoutProto.CustomName && loadout.Item2.CustomName != null)
                 _meta.SetEntityName(loadout.Item1, loadout.Item2.CustomName);
@@ -112,6 +118,12 @@ public sealed class LoadoutSystem : EntitySystem
             foreach (var function in loadoutProto.Functions)
                 function.OnPlayerSpawn(uid, loadout.Item1, _componentFactory, EntityManager, _serialization);
         }
+        #if EXCEPTION_TOLERANCE
+        catch (Exception e) // Also floofstation
+        {
+            Log.Error("Caught exception while spawning loadouts.", e)
+        }
+        #endif
 
         // Pick the heirloom
         if (heirlooms.Any())
